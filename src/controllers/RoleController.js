6ceeaf41508dd8aa21529094d983b81a63da3c2b
Marcelo Moreira <profissionalsmbr@gmail.com>
2022-06-22 01:29:32 -0300

    controllers att

--- src/controllers/RoleController.js
+++ src/controllers/RoleController.js
@@ -2,18 +2,72 @@ const Role = require('../models/RoleModel');
 
 module.exports = {
     async index(req, res) {
-        res.send('Role index');
+        try {
+            const roles = await Role.findAll();
+            res.status(200).json(roles);
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error);
+        }
     },
     async show(req, res) {
-        res.send('Role show');
+        try {
+            const id = req.params.id;
+            const role = await Role.findOne({ where: { id } });
+
+            if (!role) {
+                return res.status(400).json("Role not found!");
+            }
+
+            res.status(200).json(role);
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error);
+        }
     },
     async store(req, res) {
-        res.send('Role store');
+        try {
+            await Role.create(req.body);
+            res.status(200).json("Role creation was a success!");
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error);
+        }
     },
     async update(req, res) {
-        res.send('Role update');
+        try {
+            const { name, read, write } = req.body;
+            const id = request.params.id;
+            const role = await Role.findOne({ where: { id } });
+
+            if (!role) {
+                return res.status(400).json("Role not found!");
+            }
+
+            role.name = name;
+            role.read = read;
+            role.write = write;
+
+            await role.save();
+            res.status(200).json("Role successful updated!");
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error);
+        }
     },
     async destroy(req, res) {
-        res.send('Role destroy');
+        try {
+            const id = req.params.id;
+            const role = await Role.destroy({ where: { id } });
+
+            if (!role) {
+                return res.status(400).json("Role not found!");
+            }
+
+            res.status(200).json("Role was successful deleted!");
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error);
+        }
     }
 }
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,18 +2,73 @@ const User = require('../models/UserModel');
 
 module.exports = {
     async index(req, res) {
-        res.send('User index');
+        try {
+            const users = await User.findAll();
+            res.status(200).json(users);
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error);
+        }
     },
     async show(req, res) {
-        res.send('User show');
+        try {
+            const id = req.params.id;
+            const User = await User.findOne({ where: { id } });
+
+            if (!User) {
+                return res.status(400).json("User not found!");
+            }
+
+            res.status(200).json(User);
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error);
+        }
     },
     async store(req, res) {
-        res.send('User store');
+        try {
+            await User.create(req.body);
+            res.status(200).json("User creation successful!");
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error);
+        }
     },
     async update(req, res) {
-        res.send('User update');
+        try {
+            const { name, sex, age, role_id } = req.body;
+            const id = request.params.id;
+            const User = await User.findOne({ where: { id } });
+
+            if (!User) {
+                return res.status(400).json("User not found!");
+            }
+
+            User.name = name;
+            User.sex = sex;
+            User.age = age;
+            User.role_id = role_id;
+
+            await User.save();
+            res.status(200).json("User successful updated!");
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error);
+        }
     },
     async destroy(req, res) {
-        res.send('User destroy');
+        try {
+            const id = req.params.id;
+            const User = await User.destroy({ where: { id } });
+
+            if (!User) {
+                return res.status(400).json("User not found!");
+            }
+
+            res.status(200).json("User was successful deleted!");
+        } catch (error) {
+            console.log(error);
+            res.status(400).send(error);
+        }
     }
 }
